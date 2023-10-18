9ddfbbe187e0c00844ee16dd372116d105d2ce7a
Chingyz Arapov <chingiz12354@gmail.com>
2023-10-18 18:49:20 +0300

    comit

---
+++ Middleware в Express.js/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const v1_router = require("./router");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const HOST = "127.0.0.1";
+const PORT = 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.static("public"));
+
+app.use("/v1", v1_router);
+
+app.use(function (err, req, res, next) {
+    if (err.statusCode) {
+        res.status(err.statusCode).json(err.message);
+    } else {
+        res.status(400).json("Отправьте запрос корректно!");
+    }
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Сервер запущен http://${HOST}:${PORT}`);
+});
